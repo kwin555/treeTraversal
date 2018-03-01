d30db6c7f461475e0aae8ec7783f479e96cb0723
kwin555 <ktn555@att.net>
2018-02-28 21:49:58 -0600

    added breath depth search

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,4 +1,5 @@
-# creating a tree
+require 'queue'
+# create a tree
 class Tree
   attr_accessor :payload, :children
   def initialize(payload, children = [])
@@ -14,7 +15,32 @@ class Tree
     end
     nil
   end
+
+  def breath_first_search(node, search_value)
+    return node if payload == search_value
+    queue = MyQueue.new
+    children.each do |child|
+      return child if child.payload == search_value
+      queue.enqueue(child)
+    end
+    current_node = queue.dequeue
+    return current_node.breath_first_search(node, search_value) \
+    unless current_node.nil?
+  end
 end
 #
 # tree = Tree.new(5)
 # puts tree.depth_first_search(tree, 5).inspect
+deep_fifth_node = Tree.new(5, [])
+eleventh_node = Tree.new(11, [])
+fourth_node   = Tree.new(4, [])
+# The "Branches" of the tree
+ninth_node = Tree.new(9, [fourth_node])
+sixth_node = Tree.new(6, [deep_fifth_node, eleventh_node])
+seventh_node = Tree.new(7, [sixth_node])
+shallow_fifth_node = Tree.new(5, [ninth_node])
+
+# The "Trunk" of the tree
+trunk = Tree.new(2, [seventh_node, shallow_fifth_node])
+
+puts trunk.depth_first_search(trunk, 11).inspect
--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -1,4 +1,6 @@
 require 'spec_helper'
+require 'queue'
+require 'tree'
 
 RSpec.describe Tree, type: :model do
   describe '::initialize' do
@@ -46,9 +48,18 @@ RSpec.describe Tree, type: :model do
         end
         context ' when the node being searched for is on the right side' do
           it 'returns the node' do
-            searched = Tree.new(1)
-            tree = Tree.new(10, [Tree.new(4), searched])
-            expect(tree.depth_first_search(tree, 1)).to eq searched
+            deep_fifth_node = Tree.new(5, [])
+            eleventh_node = Tree.new(11, [])
+            fourth_node   = Tree.new(4, [])
+            # The "Branches" of the tree
+            ninth_node = Tree.new(9, [fourth_node])
+            sixth_node = Tree.new(6, [deep_fifth_node, eleventh_node])
+            seventh_node = Tree.new(7, [sixth_node])
+            shallow_fifth_node = Tree.new(5, [ninth_node])
+
+            # The "Trunk" of the tree
+            trunk = Tree.new(2, [seventh_node, shallow_fifth_node])
+            expect(trunk.depth_first_search(trunk, 4)).to eq fourth_node
           end
         end
       end
@@ -57,6 +68,56 @@ RSpec.describe Tree, type: :model do
       it 'returns nil' do
         tree = Tree.new(4)
         expect(tree.depth_first_search(tree, 5)).to be nil
+      end
+    end
+  end
+  describe '#breadth_first_search' do
+    context 'when given the searched number exists in the tree' do
+      context 'when the searched number is in the trunk' do
+        it 'returns the trunk' do
+          trunk = Tree.new(5)
+          expect(trunk.breath_first_search(trunk, 5)).to eq trunk
+        end
+      end
+      context 'when the search value is a child' do
+        context 'when the search value is on the shallow left side' do
+          it 'returns the node' do
+            searched_value = Tree.new(23)
+            tree = Tree.new(4, [searched_value, Tree.new(5)])
+            expect(tree.breath_first_search(tree, 23)).to eq searched_value
+          end
+        end
+        context 'when the search value is on the shallow right side' do
+          it 'returns the node' do
+            searched_value = Tree.new(23)
+            tree = Tree.new(4, [Tree.new(5), searched_value])
+            expect(tree.breath_first_search(tree, 23)).to eq searched_value
+          end
+        end
+        context 'when the search value is deep left' do
+          it 'should return the node' do
+            searched_value = Tree.new(5)
+            left_tree = Tree.new(7, [Tree.new(8, [searched_value])])
+            right_tree = Tree.new(19)
+            tree = Tree.new(100, [left_tree, right_tree])
+            expect(tree.breath_first_search(tree, 5)).to eq searched_value
+          end
+        end
+        context 'when the search value is deep right' do
+          it 'should return the node' do
+            searched_value = Tree.new(9)
+            left_tree = Tree.new(7, [Tree.new(6, [Tree.new(5), Tree.new(11)])])
+            right_tree = Tree.new(5, [Tree.new(9, [Tree.new(searched_value)])])
+            tree = Tree.new(2, [left_tree, right_tree])
+            expect(tree.breath_first_search(tree, 9)).to eq searched_value
+          end
+        end
+      end
+    end
+    context ' when the value being searched for is not in the tree' do
+      it 'shoud return nil' do
+        tree = Tree.new(32453)
+        expect(tree.breath_first_search(tree,5)). to be nil
       end
     end
   end
